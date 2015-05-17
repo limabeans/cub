83decc8f8b46d450aabd2e453f05505844e1e196
limabeans <toomanytypos@gmail.com>
2015-05-17 01:37:07 -0700

    basic timer working / cleaned code

--- app/src/main/java/com/angelllim/cube/cub/MainActivity.java
+++ app/src/main/java/com/angelllim/cube/cub/MainActivity.java
@@ -20,70 +20,42 @@ import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity implements OnClickListener {
-
-
     private TextView currentTimeTextView;
+
+    private long startTime = 0;
+    private boolean timeRunning = false;
+    private Handler timerHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get references
         RelativeLayout thisScreen = (RelativeLayout) findViewById(R.id.timerScreen);
-        thisScreen.setOnClickListener(this);
-
         currentTimeTextView = (TextView) findViewById(R.id.currentTime);
+
+        //Set listeners
+        thisScreen.setOnClickListener(this);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    long startTime = 0;
-    boolean timeRunning = false;
-    Runnable timerRunnable = new TimerTask() {
-        @Override
-        public void run() {
-            final long start = startTime;
-            long millis = System.currentTimeMillis() - start;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
-            seconds     = seconds % 60;
-            currentTimeTextView.setText(String.valueOf(millis));
-            timerHandler.postDelayed(this, 10);
-        }
-    };
-
-    Handler timerHandler = new Handler();
-    Timer timer = new Timer();
 
     @Override
     public void onClick(View v) {
         if (!timeRunning) {
-            timerHandler.removeCallbacks(timerRunnable);
-        } else {
             startTime = System.currentTimeMillis();
-            timerHandler.postDelayed(timerRunnable, 0);
+            timerHandler.post(timerRunnable);
+        } else {
+            timerHandler.removeCallbacks(timerRunnable);
         }
         timeRunning=!timeRunning;
     }
+
+    Runnable timerRunnable = new TimerTask() {
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            currentTimeTextView.setText(String.valueOf(millis));
+            timerHandler.post(this);
+        }
+    };
 }
