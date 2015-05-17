a21f2183039f5b03b3bbd58a4fa755dcf5c6b36a
limabeans <toomanytypos@gmail.com>
2015-05-17 13:04:27 -0700

    basic time formatting works now

--- app/src/main/java/com/angelllim/cube/cub/MainActivity.java
+++ app/src/main/java/com/angelllim/cube/cub/MainActivity.java
@@ -15,12 +15,19 @@ import android.view.View.OnClickListener;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity implements OnClickListener {
+
     private TextView currentTimeTextView;
+    private final SimpleDateFormat minutesGT10Format = new SimpleDateFormat("mm:ss.SSS");
+    private final SimpleDateFormat minutesLT10Format = new SimpleDateFormat("m:ss.SSS");
+    private final SimpleDateFormat secondsLT10Format= new SimpleDateFormat("s.SSS");
+    private final SimpleDateFormat secondsGT10Format= new SimpleDateFormat("ss.SSS");
 
     private long startTime = 0;
     private boolean timeRunning = false;
@@ -53,8 +60,22 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
     Runnable timerRunnable = new TimerTask() {
         @Override
         public void run() {
-            long millis = System.currentTimeMillis() - startTime;
-            currentTimeTextView.setText(String.valueOf(millis));
+            long millisSoFar = System.currentTimeMillis() - startTime;
+            long seconds = (millisSoFar / 1000) % 60;
+            long minutes = (millisSoFar / (1000 * 60)) % 60;
+            //long hours = (millisSoFar / (1000 * 60 * 60)) % 24;
+
+            if (minutes >= 10) {
+                currentTimeTextView.setText(minutesGT10Format.format(millisSoFar));
+            } else if (minutes >= 1) {
+                currentTimeTextView.setText(minutesLT10Format.format(millisSoFar));
+            } else if (seconds >= 10) {
+                currentTimeTextView.setText(secondsGT10Format.format(millisSoFar));
+            } else {
+                currentTimeTextView.setText(secondsLT10Format.format(millisSoFar));
+            }
+
+            //Recursively call myself again.
             timerHandler.post(this);
         }
     };
